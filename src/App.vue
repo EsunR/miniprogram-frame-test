1e60c01e44bb129294d24de556de20f69f9bd399
EsunR <641411169@qq.com>
2019-08-14 00:56:11 +0800

    git commit -m "first commit"

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  created(){
+    console.log('小程序启动了');
+  }
+};
+</script>
+
+<style>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 200rpx 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="title-container">
+      <text>顶部文字</text>
+    </div>
+  </div>
+</template>
+
+<script>
+import { list_data } from "../../data/list-data";
+export default {
+  data() {
+    return {
+      data: [],
+      refreshCount: 1
+    };
+  },
+
+  onPullDownRefresh() {
+    setTimeout(() => {
+      for (let i = 0; i < this.refreshCount; i++) {
+        this.data = this.data.concat(list_data);
+      }
+      this.refreshCount *= 2;
+      wx.stopPullDownRefresh();
+    }, 1000);
+  }
+};
+</script>
+
+<style>
+.title-container {
+  text-align: center;
+}
+</style>
+
